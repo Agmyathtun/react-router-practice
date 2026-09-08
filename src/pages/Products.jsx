65214647bed7f596e967ab9e2fc69374f2d4973a
Aung Myat Tun <143701212+Agmyathtun@users.noreply.github.com>
2026-09-08 12:42:29 +0700

    products search button add in product.jsx using useSearchParams

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,22 +1,43 @@
-import { Link } from 'react-router-dom';
+import { Link , useSearchParams} from 'react-router-dom';
 
 const products = [
-  { id: 1, name: "Wireless Headphones", price: 59.99 },
-  { id: 2, name: "Smart Watch", price: 129.99 },
-  { id: 3, name: "Coffee Maker", price: 49.99 },
-  { id: 4, name: "Backpack", price: 39.99 },
+  { id: 1, name: "Wireless Headphones", price: 59.99, category: "electronics" },
+  { id: 2, name: "Smart Watch", price: 129.99, category: "electronics" },
+  { id: 3, name: "Coffee Maker", price: 49.99, category: "home" },
+  { id: 4, name: "Backpack", price: 39.99, category: "fashion" },
 ];
+
 function Products() {
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const category = searchParams.get('category');
+  const flteredProducts = category === 'all'
+    ? products
+    : products.filter(p => p.category === category);
+  const handleFilter = (newCategory) => {
+    setSearchParams({ category: newCategory });
+  };
 
   return (
     <div style={{ padding: '40px' }}>
       <h1>Products</h1>
       <div style={{
+        display: 'flex',
+        gap: '10px',
+        margin:'20px 0'
+      }}>
+        <button onClick={ ()=> handleFilter('all')}>All</button>
+        <button onClick={ ()=> handleFilter('electronics')}>Electronics</button>
+        <button onClick={ ()=> handleFilter('home')}>Home</button>
+        <button onClick={ ()=> handleFilter('fashion')}>Fashion</button>
+
+      </div>
+
+      <div style={{
         display: 'grid',
         gap: '15px',
-        marginTop: '20px'
       }}>
-        {products.map(product => (
+        {flteredProducts.map(product => (
           <Link
             key={product.id}
             to={`/products/${product.id}`}
