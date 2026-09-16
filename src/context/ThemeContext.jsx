f99b39a75cbc180443dad22567a404015e5f5cf0
Aung Myat Tun <143701212+Agmyathtun@users.noreply.github.com>
2026-09-16 13:09:46 +0700

    Dark mode add to LoacalStorage in themeContext.jsx

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,22 +1,27 @@
-import { createContext, useState, useContext } from 'react';
+import { createContext, useState, useContext, useEffect } from "react";
 const ThemeContext = createContext();
 
 export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(() => {
+    //Load theme from localStorage
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme ? savedTheme : "dark";
+  });
+
+  //Save theme to localStorage whenever it changes
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   const toggleTheme = () => {
-    setTheme(prev => prev === 'dark' ? 'light' : 'dark')
+    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
   };
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
-
       {children}
-      
     </ThemeContext.Provider>
-      
-    
-  )
+  );
 }
 
 export function useTheme() {
